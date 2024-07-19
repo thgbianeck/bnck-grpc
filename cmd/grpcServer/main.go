2cbfe5a3c080b25d09641425447cb7004c5862db
Thaigo Bianeck <thgbianeck@gmail.com>
2024-07-19 01:10:47 -0300

    Criando servidor gRPC

---
+++ cmd/grpcServer/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"database/sql"
+	"net"
+
+	"github.com/thgbianeck/bnck-grpc/internal/database"
+	"github.com/thgbianeck/bnck-grpc/internal/pb"
+	"github.com/thgbianeck/bnck-grpc/internal/service"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "./db.sqlite")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	categoryDB := database.NewCategory(db)
+	categoryService := service.NewCategoryService(*categoryDB)
+
+	// Register gRPC server
+	grpcServer := grpc.NewServer()
+	pb.RegisterCategoryServiceServer(grpcServer, categoryService)
+	reflection.Register(grpcServer)
+
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		panic(err)
+	}
+
+	if err := grpcServer.Serve(lis); err != nil {
+		panic(err)
+	}
+}
---
+++ internal/service/category.go
@@ -0,0 +1,34 @@
+package service
+
+import (
+	"context"
+
+	"github.com/thgbianeck/bnck-grpc/internal/database"
+	"github.com/thgbianeck/bnck-grpc/internal/pb"
+)
+
+type CategoryService struct {
+	pb.UnimplementedCategoryServiceServer
+	CategoryDB database.Category
+}
+
+func NewCategoryService(categoryDB database.Category) *CategoryService {
+	return &CategoryService{
+		CategoryDB: categoryDB,
+	}
+}
+
+func (c *CategoryService) CreateCategory(ctx context.Context, in *pb.CreateCategoryRequest) (*pb.CategoryResponse, error) {
+	category, err := c.CategoryDB.Create(in.Name, in.Description)
+	if err != nil {
+		return nil, err
+	}
+	categoryResponse := &pb.CategoryResponse{
+		Category: &pb.Category{
+			Id:          category.ID,
+			Name:        category.Name,
+			Description: category.Description,
+		},
+	}
+	return categoryResponse, nil
+}
